b62e5e8ca90f63313378bfb1d3088e0d10d92ab3
Ubuntu <bitnami@ip-10-147-198-31.(none)>
2014-04-16 05:29:01 +0000

    better deploy

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ git pull
 
 cd spyr-web; make; cd ..;
 
-forever spyr-presence/index.js &
+forever start spyr-presence/index.js
 
-forever spyr-sounder/index.js &
+forever start spyr-sounder/index.js
 
-forever spyr-io/index.js &
+forever start spyr-io/index.js
 
-forever spyr-web/index.js &
+forever start spyr-web/index.js
 
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
